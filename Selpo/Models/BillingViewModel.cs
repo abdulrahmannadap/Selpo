ae3c4ff4b0c34b2e21fbb27b6eebb4f51f4cf63f
abdul <abdul@NADAP>
2025-01-31 00:46:09 +0530

    Add New View Model Class

---
+++ Selpo/Models/BillingViewModel.cs
@@ -0,0 +1,19 @@
+﻿namespace Selpo.Models
+{
+    public class BillingViewModel
+    {
+        // For multiple records
+        public IEnumerable<Company>? CompanyList { get; set; }
+        public IEnumerable<InvoicePrint>? InvoiceList { get; set; }
+        public IEnumerable<Receipt>? ReceiptList { get; set; }
+        public IEnumerable<Sender>? SenderList { get; set; }
+        public IEnumerable<TransactionDetail>? TransactionList { get; set; }
+
+        // For single records
+        public Company? Company { get; set; }
+        public InvoicePrint? Invoice { get; set; }
+        public Receipt? Receipt { get; set; }
+        public Sender? Sender { get; set; }
+        public TransactionDetail? Transaction { get; set; }
+    }
+}
